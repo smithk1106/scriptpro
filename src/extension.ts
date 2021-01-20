b228da5a8d324e23bf77f1b45cae496945ab55a1
wspre <>
2021-01-20 17:52:24 +0900

    1. Add player 2. Add format functions

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import util = require('util');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,72 +10,80 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('================ScriptPro=================');
-	console.log(context.extensionUri.fsPath);
-	console.log(context.extensionPath);
+	console.log('[D]ExtensionPath: ' + context.extensionPath);
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let formatDisposable = vscode.commands.registerCommand('scriptpro.format', () => {
 		const editor = vscode.window.activeTextEditor;
-		if(editor != null) {
+		if (editor != null) {
 			// Get selected text
 			let selectedText = '';
-			let curSelection:vscode.Selection;
+			let curRange: vscode.Range;
 
-			if(!editor.selection.isEmpty) {
+			if (!editor.selection.isEmpty) {
 				selectedText = editor.document.getText(editor.selection);
-				curSelection = editor.selection;
+				curRange = new vscode.Range(editor.selection.start, editor.selection.end);
 			} else {
 				selectedText = editor.document.getText();
-				curSelection = editor.selection;
+				curRange = new vscode.Range(
+					editor.document.lineAt(0).range.start,
+					editor.document.lineAt(editor.document.lineCount - 1).range.end
+				);
 			}
 			//[DEBUG]
-			vscode.window.showInformationMessage(selectedText);
-			let lastLine = editor.document.lineAt(editor.document.lineCount - 1);
-			editor.edit((builder:vscode.TextEditorEdit) => {
-				builder.replace(lastLine.range.end, '');
-			});
+			//vscode.window.showInformationMessage(selectedText);
 
 			// Format selected text
-			
-			
+			selectedText = selectedText.trim();
+			if (selectedText.length > 0) {
+				let formatedScript = formatScript(selectedText);
+				editor.edit((builder: vscode.TextEditorEdit) => {
+					builder.replace(curRange, formatedScript);
+				});
+			}
 		}
-		
+
 		// Display a message box to the user
 		// vscode.window.showInformationMessage('I am creating "scriptpro.format" now, please wait!');
 	});
 	context.subscriptions.push(formatDisposable);
 
 	let runDisposable = vscode.commands.registerCommand('scriptpro.run', () => {
+		const path = require("path");
 		const editor = vscode.window.activeTextEditor;
-		if(editor != null) {
+		if (editor != null) {
 			let scriptPath = editor.document.fileName;
-			if(editor.document.isDirty) {
-				vscode.window.showErrorMessage('Please save the current script before run it!');
+			if (editor.document.isDirty) {
+				vscode.window.showErrorMessage('Please save the script before run it!');
 				return;
-			} else {
-				vscode.window.showInformationMessage('Please press "Ctrl+T" to stop the script.');
 			}
 
-			// [DEBUG]
-			const path = require("path");
-			scriptPath = path.dirname(scriptPath);
-
 			// Run script
-			const process = require('child_process')
-			process.exec('ls -lash ' + scriptPath, (err:object, stdout:string, stderr:string) => {
-				console.log('stdout: ' + stdout);
-				console.log('stderr: ' + stderr);
-				if (err) {
-					console.log('error: ' + err);
-				}
+			const baseDir = context.extensionPath;
+			const localConfig = vscode.workspace.getConfiguration('scriptpro');
 
-				//[DEBUG]
-				let txt = scriptPath + '\n' + stdout;
-				let lastLine = editor.document.lineAt(editor.document.lineCount - 1);
-				editor.edit((builder:vscode.TextEditorEdit) => {
-					builder.replace(lastLine.range.end, txt);
-				});
+			//DEBUG
+			console.log('[D]scriptpro.playerPath: ' + localConfig.get<string>('playerPath'));
+			console.log('[D]scriptpro.silent: ' + localConfig.get<boolean>('silent'));
+
+			const process = require('child_process')
+			let payerPath = path.resolve(baseDir, localConfig.get<string>('playerPath', 'bin/ScriptPlayer.exe'));
+			let silentFlag = (localConfig.get<boolean>('silent', true) ? '/silent' : '');
+			let cmd: string = util.format('%s "%s" %s', payerPath, scriptPath, silentFlag);
+
+			cmd = cmd.trim()
+			vscode.window.showInformationMessage('Start ' + scriptPath);
+			vscode.window.showInformationMessage('Press "Ctrl+T" to stop the script.');
+			process.exec(cmd, (err: object, stdout: string, stderr: string) => {
+				console.log('[D]stdout: ' + stdout);
+				console.log('[D]stderr: ' + stderr);
+				if (err) {
+					console.log(err);
+					vscode.window.showErrorMessage('Failed to start script player!');
+				} else {
+					vscode.window.showInformationMessage('Stoped ' + scriptPath);
+				}
 			});
 		}
 
@@ -85,4 +94,237 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
+
+function matchAll(pattern:RegExp, text:string) {
+	let results:Array<RegExpExecArray> = [];
+	let curMatch;
+
+	while(curMatch = pattern.exec(text)) {
+		results.push(curMatch);
+	}
+
+	return results;
+}
+
+function formatCommonParams(pattern: RegExp, paramPart: string) {
+	let params: string = '';
+	let curMatch;
+
+	while (curMatch = pattern.exec(paramPart)) {
+		params += ' ' + curMatch[0];
+	}
+
+	return params;
+}
+
+function formatScript(text: string): string {
+	let formatedText: string = '';
+	let lines = text.split('\n');
+	let curIndent: string = '';
+
+	for (const i in lines) {
+		let curLine: string = lines[i].trim().replace('\t', ' ');
+		let pos: number;
+		let partAction: string, partParam: string;
+		let newLine: string = '';
+
+		if (curLine.length > 0) {
+			// Split action part and parameter part
+			pos = curLine.indexOf(' ');
+			if (pos > 0) {
+				partAction = curLine.substring(0, pos);
+				partParam = curLine.substring(pos).trim();
+			} else {
+				partAction = curLine;
+				partParam = '';
+			}
+
+			// Check and format variables and labels
+			if (partAction.startsWith('@')) {
+				const match = partParam.match(/(\:\=|\+\=|\-\=|\*\=|\%\=)\s*(.+)/);
+				if (match) {
+					newLine = util.format('%s%s %s %s', curIndent, partAction, match[1], match[2].trim());
+					//console.log('[D]' + newLine)
+				}
+			} else if (partAction.startsWith(':')) {
+				newLine = curIndent + partAction;
+			} else {
+				// Check and format actions
+				partAction = partAction.toLowerCase();
+				if (partAction == 'if') {
+					const match = partParam.match(/(.+)\s+then$/i);
+					if (match) {
+						newLine = util.format('%sIf %s Then', curIndent, match[1].trim());
+					}
+					curIndent += '\t';
+				} else if (partAction == 'elseif') {
+					const match = partParam.match(/(.+)\s+then$/i);
+					if (curIndent.length > 0) curIndent = curIndent.substring(1);
+					if (match) {
+						newLine = util.format('%sElseIf %s Then', curIndent, match[1].trim());
+					} else {
+						newLine = util.format('%sElseIf %s', curIndent, partParam);
+					}
+					curIndent += '\t';
+				} else if (partAction == 'else') {
+					if (curIndent.length > 0) curIndent = curIndent.substring(1);
+					newLine = curIndent + 'Else';
+					curIndent += '\t';
+				} else if (partAction == 'endif') {
+					if (curIndent.length > 0) curIndent = curIndent.substring(1);
+					newLine = curIndent + 'EndIf';
+				} else if (partAction == 'goto') {
+					const match = partParam.match(/\:{0,1}([a-zA-Z_]+[a-zA-Z0-9_]*)/i);
+					if (match) {
+						newLine = util.format('%sGoto :%s', curIndent, match[1]);
+					} else {
+						newLine = util.format('%sGoto %s', curIndent, partParam);
+					}
+				} else if (partAction == 'exit') {
+					newLine = curIndent + 'Exit';
+				} else if (partAction == 'delay') {
+					newLine = util.format('%sDelay %s', curIndent, partParam);
+				} else if (partAction == 'defdelay') {
+					newLine = util.format('%sDefDelay %s', curIndent, partParam);
+				} else if (partAction == 'use') {
+					newLine = util.format('%sUse %s', curIndent, partParam);
+				} else if (partAction == 'unset') {
+					newLine = curIndent + 'Unset'
+						+ formatCommonParams(/@[a-zA-Z_]+[a-zA-Z0-9_]*/g, partParam);
+				} else if (partAction == 'setreturn') {
+					newLine = curIndent + 'SetReturn'
+						+ formatCommonParams(/@[a-zA-Z_]+[a-zA-Z0-9_]*/g, partParam);
+				} else if (partAction == 'random') {
+					newLine = curIndent + 'Random'
+						+ formatCommonParams(/@[a-zA-Z_]+[a-zA-Z0-9_]*|[0-9\.]+/g, partParam);
+				} else if (partAction == 'call') {
+					newLine = curIndent + 'Call'
+						+ formatCommonParams(/"[^"]*"|@[a-zA-Z_]+[a-zA-Z0-9_]*|[0-9\.]+/g, partParam);
+				} else if (partAction == 'include') {
+					newLine = curIndent + 'Include'
+						+ formatCommonParams(/"[^"]*"|@[a-zA-Z_]+[a-zA-Z0-9_]*/g, partParam);
+				} else if (partAction == 'onerror') {
+					const match = partParam.match(/(resume|goto|exit)\s*(.*)/i);
+					if (match) {
+						if (match[1].toLowerCase() == 'resume') {
+							newLine = curIndent + 'OnError Resume Next';
+						} else if (match[1].toLowerCase() == 'resume') {
+							newLine = util.format('%sOnError Goto %s', curIndent, match[2].trim());
+						} else {
+							newLine = curIndent + 'OnError Exit';
+						}
+					}
+				} else if (partAction == 'invoke') {
+					newLine = curIndent + 'Invoke'
+						+ formatCommonParams(/"[^"]*"|@[a-zA-Z_]+[a-zA-Z0-9_]*|[0-9\.]+/g, partParam);
+				} else if (partAction == 'msgbox') {
+					newLine = curIndent + 'MsgBox'
+						+ formatCommonParams(/"[^"]*"|@[a-zA-Z_]+[a-zA-Z0-9_]*|[0-9\.]+/g, partParam);
+				} else if (partAction == 'run') {
+					newLine = curIndent + 'Run'
+						+ formatCommonParams(/"[^"]*"|@[a-zA-Z_]+[a-zA-Z0-9_]*|[0-9\.]+/g, partParam);
+				} else if (partAction == 'runandwait') {
+					newLine = curIndent + 'RunAndWait'
+						+ formatCommonParams(/"[^"]*"|@[a-zA-Z_]+[a-zA-Z0-9_]*|[0-9\.]+/g, partParam);
+				} else if (partAction == 'monitor') {
+					if (partParam.toLowerCase() == 'on') {
+						newLine = curIndent + 'Monitor On';
+					} else {
+						newLine = curIndent + 'Monitor Off';
+					}
+				} else if (partAction == 'messagemode') {
+					if (partParam.toLowerCase() == 'on') {
+						newLine = curIndent + 'MessageMode On';
+					} else {
+						newLine = curIndent + 'MessageMode Off';
+					}
+				} else if (partAction == 'checkpixel') {
+					newLine = curIndent + 'CheckPixel'
+						+ formatCommonParams(/#[0-9a-f]+|@{0,1}[a-zA-Z_]+[a-zA-Z0-9_]*|-{0,1}[0-9]+/g, partParam);
+				} else if (partAction == 'findpixel') {
+					newLine = curIndent + 'FindPixel'
+						+ formatCommonParams(/#[0-9a-f]+|@{0,1}[a-zA-Z_]+[a-zA-Z0-9_]*|-{0,1}[0-9]+/g, partParam);
+				} else if (partAction == 'findmodel') {
+					newLine = curIndent + 'FindModel'
+						+ formatCommonParams(/"[^"]*"|@{0,1}[a-zA-Z_]+[a-zA-Z0-9_]*|-{0,1}[0-9]+/g, partParam);
+				} else if (partAction == 'capture') {
+					newLine = curIndent + 'Capture'
+						+ formatCommonParams(/"[^"]*"|@{0,1}[a-zA-Z_]+[a-zA-Z0-9_]*|-{0,1}[0-9]+/g, partParam);
+				} else if (partAction == 'key') {
+					newLine = curIndent + 'Key'
+						+ formatCommonParams(/down|up|press|[a-zA-Z0-9]+/g, partParam);
+				} else if (partAction == 'sendkeys') {
+					newLine = util.format('%sSendKeys %s', curIndent, partParam);
+				} else if (partAction == 'settext') {
+					newLine = util.format('%sSetText %s', curIndent, partParam);
+				} else if (partAction == 'gettext') {
+					newLine = util.format('%sGetText %s', curIndent, partParam);
+				} else if (partAction == 'origin') {
+					const matches = matchAll(/(from|point|null|LT|LB|RT|RB|TL|BL|TR|BR)|"[^"]*"|@[a-zA-Z_]+[a-zA-Z0-9_]*|-{0,1}[0-9]+/ig, partParam);
+					
+					newLine = curIndent + 'Origin';
+					if (matches.length > 1) {
+						if(matches[0][1] && matches[0][1].toLowerCase() == 'from'){
+							newLine += ' From';
+							if(matches[1][1] && matches[1][1].toLowerCase() == 'point'){
+								newLine += ' Point';
+								pos = 2;
+							} else {
+								pos = 1;
+							}
+						} else {
+							pos = 0;
+						}
+
+						for(; pos < matches.length; pos++){
+							if(matches[pos][1] && matches[pos][1].length > 0) {
+								if(matches[pos][1].toLowerCase() == 'null') {
+									newLine += ' Null';
+								} else {
+									newLine += ' ' + matches[pos][1].toUpperCase();
+								}
+							} else {
+								newLine += ' ' + matches[pos][0];
+							}
+						}
+					} else {
+						newLine += formatCommonParams(/"[^"]*"|@{0,1}[a-zA-Z_]+[a-zA-Z0-9_]*|[0-9]+/g, partParam);
+					}
+				} else if (partAction == 'mouse') {
+					const match = partParam.match(/(move|left|right|middle)\s+(down|up|click|doubleclick){0,1}\s*(at|offset){0,1}\s*(-{0,1}\d*)[ \t,]*(-{0,1}\d*)/i);
+					if (match) {
+						//console.log(match);	//DEBUG
+						newLine = util.format('%sMouse %s%s', curIndent, match[1].charAt(0).toUpperCase(), match[1].substring(1).toLowerCase());
+						if (match[2] != null && match[2].length > 0) {
+							newLine += ' ' + match[2].charAt(0).toUpperCase() + match[2].substring(1).toLowerCase();
+						}
+						if (match[3] != null && match[3].length > 0) {
+							newLine += ' ' + match[3].charAt(0).toUpperCase() + match[3].substring(1).toLowerCase();
+						}
+						if (match[4] != null && match[4].length > 0) {
+							newLine += ' ' + match[4];
+						}
+						if (match[5] != null && match[5].length > 0) {
+							newLine += ' ' + match[5];
+						}
+					} else {
+						newLine = util.format('%sMouse %s', curIndent, partParam);
+					}
+				}
+			}
+
+			// If current line can't be formated, then ...
+			if (newLine.length < 1) {
+				newLine = util.format('%s%s', curIndent, curLine);
+			}
+
+			// Add to formated text
+			formatedText += newLine + '\n';
+		} else {
+			formatedText += '\n';
+		}
+	}
+
+	return formatedText;
+}
